4307bb761171ad7782b07b03025bfe7ee1352b6c
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-07-06 12:09:41 +0300

    shows on the sides

--- index.js
+++ index.js
@@ -19,77 +19,86 @@ function getCharacterNames(characterName){
     characterName.forEach(characterName =>{
         let nameDiv = document.createElement('div');
         nameDiv.classList ="characterName"
-        nameDiv.innerText = `-${characterName.name}`
-        characterList.appendChild(nameDiv)
+        nameDiv.innerText = `-${characterName.name}`;
+        characterList.appendChild(nameDiv);
         nameDiv.addEventListener('click', () =>{
-            fetchCharacterDetails(characterName);
-            //function for displaying on the #character-details
-        })
-    })
+           fetchCharacterDetails(characterName);
+        //function for displaying on the #character-details
+       })
+   })
 };
 
 //this is to get character details but to display we need to call the fetch function
 
 //function to display characaterDetails onto the character details on the #character-details tag 
-function fetchCharacterDetails(characterName){
-    fetch(`http://localhost:3000/characters/${characterName.id}`)
+function fetchCharacterDetails(character){
+    fetch(`http://localhost:3000/characters/${character.id}`)
     .then(resp => resp.json())
     .then(data =>{
         displayCharacterDetails(data)
     })
 }
 
-//fucntion to display the details now
-function displayCharacterDetails(details){
+//function to display the details now
+function displayCharacterDetails(character){
     let characaterDetails = document.getElementById('character-details');
+    characaterDetails.innerHTML = "<p>This character is:<p>";
 
-    details.forEach(details => {
+
         let displayDivMain = document.createElement('div');
         displayDivMain.classList = 'displayedDetailsMain';
+        //displayDivMain.innerHTML = '<p>paragh<p>';
 
         //displays name 
         let displayDivName = document.createElement('div');
             displayDivName.classList = 'displayedDetailsChild';
             displayDivName.id = 'displayedDetailsName';
-            displayDivName.inneText = `${details.name}`
+            displayDivName.innerText = `${character.name}`
             displayDivMain.appendChild(displayDivName);
+            console.log(displayDivName);
 
-        //displays status 
-        let displayDivStatus = document.createElement('div');
-            displayDivStatus.classList = 'displayedDetailsChild';
-            displayDivStatus.id = 'displayedDetailsStatus';
-            displayDivStatus.inneText = `${details.name}`
-            displayDivMain.appendChild(displayDivStatus);
         
-        // //displays type 
-        // let displayDivChild = document.createElement('div');
-        // displayDivChild.classList = 'displayedDetailsChild';
-        // displayDivChild.inneText = details.name
-        // characaterDetails.appendChild(displayDivChild)  
+        //displays status 
+            let displayDivStatus = document.createElement('div');
+                displayDivStatus.classList = 'displayedDetailsChild';
+                displayDivStatus.id = 'displayedDetailsStatus';
+                displayDivStatus.innerText = `${character.status}`
+                displayDivMain.appendChild(displayDivStatus);
+            
+        //displays type 
+            let displayDivType = document.createElement('div');
+                displayDivType.classList = 'displayedDetailsChild';
+                displayDivType.id = 'displayedDetailsType';
+                displayDivType.innerText = `${character.type}`
+                displayDivMain.appendChild(displayDivType);  
 
 
-        // //displays species 
-        // let displayDivChild = document.createElement('div');
-        // displayDivChild.classList = 'displayedDetailsChild';
-        // displayDivChild.inneText = details.name
-        // characaterDetails.appendChild(displayDivChild)  
+        //displays species 
+            let displayDivSpecies = document.createElement('div');
+                displayDivSpecies.classList = 'displayedDetailsChild';
+                displayDivSpecies.id = 'displayedDetailsSpecies';
+                displayDivSpecies.innerText = `${character.species}`
+                displayDivMain.appendChild(displayDivSpecies);  
 
-        // //displays gender 
-        // let displayDivChild = document.createElement('div');
-        // displayDivChild.classList = 'displayedDetailsChild';
-        // displayDivChild.inneText = details.name
-        // characaterDetails.appendChild(displayDivChild)  
+        //displays gender 
+            let displayDivGender = document.createElement('div');
+                displayDivGender.classList = 'displayedDetailsChild';
+                displayDivGender.id = 'displayedDetailsGender';
+                displayDivGender.innerText = `${character.gender}`
+                displayDivMain.appendChild(displayDivGender);
 
-        // //displays origin 
-        // let displayDivChild = document.createElement('div');
-        // displayDivChild.classList = 'displayedDetailsChild';
-        // displayDivChild.inneText = details.name
-        // characaterDetails.appendChild(displayDivChild)  
+        //displays origin 
+            let displayDivOrigin = document.createElement('div');
+                displayDivOrigin.classList = 'displayedDetailsChild';
+                displayDivOrigin.id = 'displayedDetailsOrigin';
+                displayDivOrigin.innerText = `${character.origin}`
+                displayDivMain.appendChild(displayDivOrigin); 
 
-        characaterDetails.appendChild(displayDivMain)
+    characaterDetails.appendChild(displayDivMain)
+    console.log(character)
+};
 
-    })
-}
+
 
 
 
